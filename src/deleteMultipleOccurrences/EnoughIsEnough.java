8548001f751fb294b2e893e33dbf14338722d5c9
Jamie95187 <jamiewong63@googlemail.com>
2020-04-07 21:39:09 +0100

    test: input = ({2},1)

--- src/deleteMultipleOccurrences/EnoughIsEnough.java
+++ src/deleteMultipleOccurrences/EnoughIsEnough.java
@@ -3,7 +3,7 @@ package deleteMultipleOccurrences;
 public class EnoughIsEnough {
 	
 	static int[] deleteNth(int[] elements, int maxOccurrences) {
-		return new int[] {1};
+		return elements;
 	}
 	
 }
--- src/deleteMultipleOccurrences/EnoughIsEnoughTest.java
+++ src/deleteMultipleOccurrences/EnoughIsEnoughTest.java
@@ -10,5 +10,10 @@ public class EnoughIsEnoughTest {
 	public void deleteNth_singleElementArray_returnOriginalArray() {
 		assertArrayEquals(new int [] {1}, EnoughIsEnough.deleteNth(new int [] {1}, 1));
 	}
+	
+	@Test
+	public void deleteNth_differentSingleElementArray_returnOriginalArray() {
+		assertArrayEquals(new int [] {2}, EnoughIsEnough.deleteNth(new int [] {2}, 1));
+	}
 
 }
