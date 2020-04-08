970e8efdcb8e382b174e36a5c0aa4235b23a487c
Jamie95187 <jamiewong63@googlemail.com>
2020-04-08 04:25:31 +0100

    test: input maxOccurrences of 4

--- src/deleteMultipleOccurrences/EnoughIsEnough.java
+++ src/deleteMultipleOccurrences/EnoughIsEnough.java
@@ -1,12 +1,40 @@
 package deleteMultipleOccurrences;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 public class EnoughIsEnough {
 	
 	static int[] deleteNth(int[] elements, int maxOccurrences) {
-		if(elements.length == 2 && maxOccurrences == 1) {
-			return new int[] {elements[0]};
+		HashMap<Integer, Integer> elementCount = new HashMap<Integer, Integer>();
+		LinkedList<Integer> fifoElements = new LinkedList<Integer>();
+		for(int i = 0; i < elements.length; i++) {
+			if(elementCount.containsKey((int)elements[i])) {
+				if(elementCount.get(elements[i]) < maxOccurrences) {
+					elementCount.put(elements[i], elementCount.get(elements[i])+1);
+					fifoElements.add(elements[i]);
+//					System.out.println("HENLO");
+				}
+			} else {
+				if(!elementCount.containsKey(elements[i])) {
+					elementCount.put(elements[i], 1);
+					fifoElements.add(elements[i]);
+				}
+			}
 		}
-		return elements;
+//		for(Map.Entry<Integer, Integer> me : elementCount.entrySet()) {
+//			System.out.println("Key: " + me.getKey());
+//			System.out.println("Value: " + me.getValue());
+//		}
+		int[] answer = new int[fifoElements.size()];
+		int index = 0;
+		while(!fifoElements.isEmpty()) {
+			answer[index] = fifoElements.removeFirst();
+			index++;
+		}
+		return answer;
+
 	}
 	
 }
--- src/deleteMultipleOccurrences/EnoughIsEnoughTest.java
+++ src/deleteMultipleOccurrences/EnoughIsEnoughTest.java
@@ -9,17 +9,31 @@ public class EnoughIsEnoughTest {
 	@Test
 	public void deleteNth_singleElementArray_returnOriginalArray() {
 		assertArrayEquals(new int [] {1}, EnoughIsEnough.deleteNth(new int [] {1}, 1));
-	}
-	
-	@Test
-	public void deleteNth_differentSingleElementArray_returnOriginalArray() {
 		assertArrayEquals(new int [] {2}, EnoughIsEnough.deleteNth(new int [] {2}, 1));
 	}
 	
 	@Test
 	public void deleteNth_maxOccurrencesOf1_returnCorrectArray() {
-		assertArrayEquals(new int [] {1}, EnoughIsEnough.deleteNth(new int [] {1, 1}, 1));
-		assertArrayEquals(new int [] {2}, EnoughIsEnough.deleteNth(new int [] {2, 2}, 1));
+		assertArrayEquals(new int [] {1}, EnoughIsEnough.deleteNth(new int [] {1,1}, 1));
+		assertArrayEquals(new int [] {2}, EnoughIsEnough.deleteNth(new int [] {2,2}, 1));
 	}
-
+	
+	@Test
+	public void deleteNth_maxOccurrencesOf2_returnCorrectArray() {
+		assertArrayEquals(new int [] {1,1}, EnoughIsEnough.deleteNth(new int [] {1,1,1}, 2));
+		assertArrayEquals(new int [] {1,2,1}, EnoughIsEnough.deleteNth(new int [] {1,2,1}, 2));
+	}
+	
+	@Test
+	public void deleteNth_maxOccurrencesOf3_returnCorrectArray() {
+		assertArrayEquals(new int [] {1,1,1}, EnoughIsEnough.deleteNth(new int [] {1,1,1,1}, 3));
+		assertArrayEquals(new int [] {1,2,1,2,1,2}, EnoughIsEnough.deleteNth(new int [] {1,2,1,2,1,2,1,1,2,2}, 3));
+	}
+	
+	@Test
+	public void deleteNth_maxOccurrencesOf4_returnCorrectArray() {
+		assertArrayEquals(new int [] {1,3,2,3,3,3,4,5,1,1,1,4,4,4}, EnoughIsEnough.deleteNth(new int [] {1,3,2,3,3,3,3,4,5,1,3,3,1,1,4,1,1,3,4,4,1,3}, 4));
+		assertArrayEquals(new int [] {1,1,1,1}, EnoughIsEnough.deleteNth(new int [] {1,1,1,1,1,1}, 4));
+	}
+	
 }
